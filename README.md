1169defa3c8173e3f2a1ec36643187ffc86be41a
dougles <douglespereira@gmail.com>
2023-05-03 15:28:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# trabajo_practico
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-nrpcjh)
---
+++ index.js
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+console.log(d3);
+d3.csv(
+  'https://raw.githubusercontent.com/dougles/AG3_Visualizacion/main/AG3_Datos_Caja_Bigotes.txt',
+  function (err, data) {}
+);
